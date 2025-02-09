34559314efd0b2a6256f6b61eae889357057b8be
Artem Hontar iMac <artsiom.hontar@ihsmarkit.com>
2025-02-09 11:52:09 +0100

    feat: add telegram

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Navigation from "./components/Navigation/Navigation";
 import Home from "./components/Home/Home";
@@ -11,10 +11,28 @@ import MagicBall from "./components/Cards/MagicBall/MagicBall";
 import Tarot from "./components/Cards/Tarot/Tarot";
 import Runes from "./components/Cards/Runes/Runes";
 import Affirmations from "./components/Affirmations/Affirmations";
+import { retrieveLaunchParams } from "@telegram-apps/sdk";
 
 const App: React.FC = () => {
+  const [telegramData, setTelegramData] = useState<string>("");
+
+  useEffect(() => {
+    // Extract initDataRaw
+    const { initDataRaw } = retrieveLaunchParams();
+
+    if (typeof initDataRaw === "string") {
+      setTelegramData(initDataRaw);
+    } else {
+      console.error("initDataRaw is not a string", initDataRaw);
+    }
+  }, []);
+
   return (
     <div className={styles.app}>
+      <div className={styles.telegramData}>
+        <h3>Telegram Data:</h3>
+        <pre>{telegramData}</pre>
+      </div>
       <Router>
         <div className={styles.content}>
           <Routes>
