7e5d8aaf4eb6da17bafa140d97c19fd6524d9202
Artem Hontar iMac <artemgontar16@gmail.com>
2025-04-13 12:28:07 +0200

    feat: banner

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -11,7 +11,7 @@ interface BannerProps {
 const Banner: React.FC<BannerProps> = ({ headerText, subText, bgColor, icon }) => {
   return (
     <div className={`${styles.banner} ${bgColor}`}>
-      <div className="flex items-center justify-center mb-2">
+      <div className="flex items-center justify-center mb-4">
         {icon}
       </div>
       <h2 className={styles.bannerText}>{headerText}</h2>
