b5a91626276970390de85108a0a62d38f6a7b0be
Artem Hontar iMac <artemgontar16@gmail.com>
2025-04-30 11:49:46 +0200

    feat: analytics

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,12 @@ import './i18n';
 import { AppRoot } from '@telegram-apps/telegram-ui'
 import '@telegram-apps/telegram-ui/dist/styles.css';
 import "./styles/theme.css";
+import telegramAnalytics from '@telegram-apps/analytics';
+
+telegramAnalytics.init({
+    token: 'eyJhcHBfbmFtZSI6ImF1cmFfYW5hbHl0aWNzIiwiYXBwX3VybCI6Imh0dHBzOi8vdC5tZS9hdXJhX2ZvcnR1bmVfYm90IiwiYXBwX2RvbWFpbiI6Imh0dHBzOi8vYXVyYS11aS52ZXJjZWwuYXBwLyJ9!BS9oUr1Qmzi2bW5wbcxUp1Ls30MdBZxjFXKUU705Vxk=', // SDK Auth token received via @DataChief_bot
+    appName: 'aura_analytics', // The analytics identifier you entered in @DataChief_bot
+});
 
 createRoot(document.getElementById('root')!).render(
   <AppRoot id="telegram-root">
